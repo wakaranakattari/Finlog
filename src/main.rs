479cfe362d098101fc13a18fdcfe00686dd8e11d
Nikita <wakaranakattari@gmail.com>
2026-06-15 15:35:56 +0500

    refactor: application's core business logic has been moved into separate modules, the CLI menu styling has been changed for a more enjoyable user experience, the application now stores data in JSON format, and the CLI version now has the ability to work asynchronously with the web server

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,18 @@
 use chrono::Local;
 use finlog::utils::*;
+use finlog::{core::SpendingManager, server::run_server};
 use std::io::{self, Write};
-use std::process;
 use std::thread;
 use std::time::Duration;
 
-// ================================ //
-//            UTILITIES             //
-// ================================ //
-
-/// Clears terminal including scrollback history
-fn clear_console() {
-    print!("\x1B[2J\x1B[3J\x1B[H");
-    io::stdout().flush().unwrap();
-}
-
-/// Clears screen and exits with Magenta message
-fn exit() {
-    clear_console();
-    println!("{}", color_print("Goodbye!", Color::Magenta));
-    process::exit(0);
-}
-
-fn header() {
-    println!(
-        "{}",
-        color_print("┌────────────────────────────────────────┐", Color::Green)
-    );
-    println!(
-        "{}",
-        color_print("│                 Finlog                 │", Color::Yellow)
-    );
-    println!(
-        "{}",
-        color_print("└────────────────────────────────────────┘", Color::Red)
-    );
-}
-
-// ================================ //
-//             CORE LOGIC           //
-// ================================ //
-
-/// Adds a spending item to the vector
-fn spending_item(spending: &mut Vec<String>) {
-    let mut item = String::new();
-
-    clear_console();
-
-    print!("Enter a spending item: ");
-    io::stdout().flush().unwrap();
-    io::stdin()
-        .read_line(&mut item)
-        .expect(&color_error_print("Error reading line"));
-
-    spending.push(item.trim().to_string());
-
-    print!(
-        "{}",
-        color_info_print(&format!(
-            "Spending item added: {}",
-            spending[spending.len() - 1]
-        ))
-    );
-    thread::sleep(Duration::from_millis(1500));
-    clear_console();
-}
-
-/// Displays all spending items
-fn view_spending_items(spending: &[String]) {
-    clear_console();
-    println!("Your spending items:");
-    for (index, item) in spending.iter().enumerate() {
-        println!("{}. {}", index + 1, item);
-    }
-
-    print!("\n<- Back to main menu");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut String::new()).unwrap();
-    // thread::sleep(Duration::from_millis(1500));
-    clear_console();
-}
-
 /// Main menu handler
-fn menu(spending: &mut Vec<String>) {
+async fn menu(spending: &mut SpendingManager) {
     let now = Local::now();
 
     header();
 
     println!(
-        "{}{}",
+        "\n{}{}",
         color_print("Time: ", Color::Cyan),
         now.format("%H:%M:%S")
     );
@@ -99,9 +23,19 @@ fn menu(spending: &mut Vec<String>) {
         now.format("%d.%m.%Y")
     );
 
-    println!("\n1. Add spending item");
-    println!("2. View spending items");
-    println!("3. Exit");
+    println!("{}, Nikita!\n", color_print("Good evening", Color::Yellow));
+
+    println!("┌────────────────────────────────────────┐");
+    println!("│ 1. Start WEB Version                   │");
+    println!("│ 2. Add spending item                   │");
+    println!("│ 3. View spending items                 │");
+    println!("│ 4. Calculate percentage of total       │");
+    println!("│ 5. Show statistics                     │");
+    println!("│ 6. Find item by name                   │");
+    println!("│ 7. Delete item                         │");
+    println!("│ 8. Settings                            │");
+    println!("│ 9. Exit                                │");
+    println!("└────────────────────────────────────────┘");
 
     print!("\n> ");
     io::stdout().flush().unwrap();
@@ -112,23 +46,51 @@ fn menu(spending: &mut Vec<String>) {
         .expect(&color_error_print("Error read line"));
 
     match input.trim() {
-        "1" => spending_item(spending),
-        "2" => view_spending_items(spending),
-        "3" => exit(),
-        _ => println!("{}", color_error_print("Invalid choice")),
+        "1" => {
+            tokio::spawn(async {
+                run_server(3000).await;
+            });
+
+            tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+
+            println!("\n<- Press Enter to back to main menu");
+            let mut dummy = String::new();
+            io::stdin().read_line(&mut dummy).unwrap();
+
+            clear_console();
+        }
+        "2" => spending.add_spending_item(),
+        "3" => spending.view_spending_items(),
+        //  Coming soon methods
+        // "4" => spending.calculate_percentage_of_total(),
+        // "5" => spending.show_statistics(),
+        // "6" => spending.find_item_by_name(),
+        // "7" => spending.delete_spending_item(),
+        // "8" => spending.settings(),
+        "9" => exit(),
+        "" => {
+            if input.trim().is_empty() {
+                return;
+            }
+        }
+        _ => {
+            clear_console();
+            println!("{}", color_error_print("Coming soon"));
+            thread::sleep(Duration::from_millis(1500));
+        }
     }
 }
 
-// ================================ //
-//              ENTRY               //
-// ================================ //
+// ┌──────────────────────────────┐ //
+// │            ENTRY             │ //
+// └──────────────────────────────┘ //
 
-fn main() {
-    let mut spending: Vec<String> = Vec::new();
-
+#[tokio::main]
+async fn main() {
     clear_console();
+    let mut spending = SpendingManager::new();
 
     loop {
-        menu(&mut spending);
+        menu(&mut spending).await;
     }
 }
